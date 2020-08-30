7e29a307dcebb930682e39180c236c6be0120113
wison <303404405@qq.com>
2020-08-30 18:38:07 +0800

    配置静态资源cdn

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     lintOnSave: false, // 取消 eslint 验证
     assetsDir:'static',
-    publicPath: process.env === 'prodction'? 'http//cdn.vson.top':'/'
+    publicPath: process.env === 'development'? '/':'//cdn.vson.top',
+    productionSourceMap: false
 }
